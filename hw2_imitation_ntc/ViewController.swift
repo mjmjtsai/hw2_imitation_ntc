51b01fdec3f0cbf1997cd3151ad677a2680556e9
mjmjtsai <mjmjtsai@gmail.com>
2019-07-15 14:35:51 +0800

    Initial Commit

---
+++ hw2_imitation_ntc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hw2_imitation_ntc
+//
+//  Created by admin on 2019/7/14.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
